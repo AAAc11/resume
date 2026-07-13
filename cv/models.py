edd83849ab56e111322b9725f6d7ac31814fcfb2
Anna <annacygan344@gmail.com>
2026-07-13 16:52:57 +0200

    Add Skills model, Add About me model, Add Certifications model, Add Project model, Add Volunteer model

--- cv/models.py
+++ cv/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Education(models.Model):
-    name_of_institution = models.CharField(max_length=50)
+    institution_name = models.CharField(max_length=100)
     start_year = models.IntegerField()
     finish_year = models.IntegerField(blank=True, null=True)
 
@@ -10,14 +10,65 @@ class Education(models.Model):
     
 
 class Experience(models.Model):
-    employer = models.CharField(max_length=50)
-    position_held_at_work = models.CharField(max_length=50)
-    place_of_work = models.CharField(max_length=50)
+    employer = models.CharField(max_length=100)
+    position_held_at_work = models.CharField(max_length=100)
+    place_of_work = models.CharField(max_length=100)
     start_date = models.DateField()
     finish_date = models.DateField()
-    work_description = models.TextField(max_length=400)
+    work_description = models.TextField(max_length=1000)
 
 
     def __str__(self):
         return self.employer
     
+class Skills(models.Model):
+    skill_name = models.CharField(max_length=100)
+    category_choices = [
+        ("Cognitive skills", "Cognitive skills"),
+        ("Technical skills", "Technical skills"),
+        ("Creative skills", "Creative skills"),
+        ("Managment skills", "Managment skills"),
+    ]
+    skill_category = models.CharField(choices=category_choices)
+
+    def __str__(self):
+        return self.skill_name
+    
+class AboutMe(models.Model):
+    about_me_description = models.TextField(max_length=2000)
+
+    def __str__(self):
+        return self.about_me_description
+    
+class Certifications(models.Model):
+    certification_name = models.CharField(max_length=100)
+    issuing_institution = models.CharField(max_length=100)
+    date_of_issue = models.DateField()
+    gained_skills = models.CharField(blank=True, null=True)
+    certification_description = models.TextField(max_length=1000)
+    credential_ID = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.certification_name
+
+class Projects(models.Model):
+    project_name = models.CharField(max_length=100)
+    project_description = models.TextField(max_length=2000)
+    creators = models.CharField(max_length=200, blank=True, null=True)
+    used_tools = models.TextField(max_length=500)
+    start_date = models.DateField()
+    finish_date = models.DateField()
+    project_link = models.URLField()
+
+    def __str__(self):
+        return self.project_name
+    
+class VolunteerExperience(models.Model):
+    volunteer_role = models.CharField(max_length=100)
+    institution_name = models.CharField(max_length=100)
+    volunteering_start_date = models.DateField()
+    volunteering_finish_date = models.DateField()
+    description_of_volunteering = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return self.volunteer_role
--- cv/views.py
+++ cv/views.py
@@ -1,9 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Education, Experience
+from cv import models
 
 def index(request):
-    education_data = Education.objects.all()
-    experience_data = Experience.objects.all()
-    data_dict = {"education_elements": education_data, "experience_elements": experience_data}
+    education_data = models.Education.objects.all()
+    experience_data = models.Experience.objects.all()
+    skills_data = models.Skills.objects.all()
+    about_me_data = models.AboutMe.objects.all()
+    certifications_data = models.Certifications.objects.all()
+    projects_data = models.Projects.objects.all()
+    volunteer_experience_data = models.VolunteerExperience.objects.all()
+
+    data_dict = {
+        "education_elements": education_data,
+        "experience_elements": experience_data,
+        "skill_elements": skills_data,
+        "about_me_elements": about_me_data,
+        "certifications_elements": certifications_data,
+        "projects_elements": projects_data,
+        "volunteer_experience_elements": volunteer_experience_data,
+        }
+    
     return render(request, "cv/home_page.html", data_dict)
