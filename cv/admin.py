b411a59dfcdfee491d21501ce69ee85c45b923c2
Anna <annacygan344@gmail.com>
2026-07-13 16:52:57 +0200

    Add Skills model, Add About me model, Add Certifications model, Add Project model, Add Volunteer model

--- cv/admin.py
+++ cv/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Education, Experience
+from cv import models
 
-admin.site.register(Education)
-admin.site.register(Experience)
+admin.site.register(models.Education)
+admin.site.register(models.Experience)
+admin.site.register(models.Skills)
+admin.site.register(models.AboutMe)
+admin.site.register(models.Certifications)
+admin.site.register(models.Projects)
+admin.site.register(models.VolunteerExperience)
