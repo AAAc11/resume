db5b6762f4305c4f55f69394d417fef963471a52
Anna <annacygan344@gmail.com>
2026-07-24 12:47:06 +0200

    Add Contact section; Delete models; Change Projects model

--- cv/views.py
+++ cv/views.py
@@ -1,24 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from cv import models
+from .models import Projects
 
 def index(request):
-    education_data = models.Education.objects.all()
-    experience_data = models.Experience.objects.all()
-    skills_data = models.Skills.objects.all()
-    about_me_data = models.AboutMe.objects.all()
-    certifications_data = models.Certifications.objects.all()
-    projects_data = models.Projects.objects.all()
-    volunteer_experience_data = models.VolunteerExperience.objects.all()
-
-    data_dict = {
-        "education_elements": education_data,
-        "experience_elements": experience_data,
-        "skill_elements": skills_data,
-        "about_me_elements": about_me_data,
-        "certifications_elements": certifications_data,
-        "projects_elements": projects_data,
-        "volunteer_experience_elements": volunteer_experience_data,
-        }
+    projects_data = Projects.objects.all().order_by('display_order')
     
-    return render(request, "cv/home_page.html", data_dict)
+    return render(request, "cv/home_page.html", {'projects': projects_data})
