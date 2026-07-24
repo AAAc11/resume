e50bd49551d28432cbe2fe8eb159d7d0c4ccdbce
Anna <annacygan344@gmail.com>
2026-07-24 12:47:06 +0200

    Add Contact section; Delete models; Change Projects model

--- cv/admin.py
+++ cv/admin.py
@@ -1,10 +1,4 @@
 from django.contrib import admin
 from cv import models
 
-admin.site.register(models.Education)
-admin.site.register(models.Experience)
-admin.site.register(models.Skills)
-admin.site.register(models.AboutMe)
-admin.site.register(models.Certifications)
 admin.site.register(models.Projects)
-admin.site.register(models.VolunteerExperience)
