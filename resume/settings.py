556f4cefab5fd4a2b57be32edc267da62dee092f
Anna <annacygan344@gmail.com>
2026-08-03 18:01:33 +0200

    Prepare for deployment

--- resume/settings.py
+++ resume/settings.py
@@ -24,9 +24,9 @@ from decouple import config
 
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['annacygan.pythonanywhere.com']
 
 
 # Application definition
@@ -117,3 +117,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
